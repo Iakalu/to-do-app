e9beff059cd045a42c1e61481b424c881a096b63
Iakalu <151212315+Iakalu@users.noreply.github.com>
2025-03-24 19:50:10 +0700

    tamthoi

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,29 @@ Route::post('tasks', function(Request $request){
     $task->completed = false;
     $task->save();
     return redirect()->route('tasks.index')->with('success', 'Task created successfully');
-})->name('tasks.create');
+})->name('tasks.store');
+
+Route::get('tasks/{id}/edit', function($id){
+    $task = Task::findOrFail($id);
+    return view('edit', ['task'=>$task]);
+})->name('tasks.edit');
+
+Route::put('tasks/{id}', function($id, Request $request){
+    $data = $request->validate([
+        'title'=>'required|max:255',
+        'description'=>'required|min:3|max:255',
+        'long_description'=>'required|min:3|max:255',
+    ]);
+    $task = Task::findOrFail($id);
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+    return redirect()->route('tasks.index')->with('success', 'Task updated successfully');
+})->name('tasks.update');
+
+Route::delete('tasks/{id}', function ($id) {
+    $task = Task::findOrFail($id);
+    $task->delete();
+    return redirect()->route('tasks.index')->with('success', 'Task deleted successfully');
+})->name('tasks.delete');

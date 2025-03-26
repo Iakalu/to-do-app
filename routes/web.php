38cbf2aa62f6072611377817bd099b2d352a6fe1
Iakalu <151212315+Iakalu@users.noreply.github.com>
2025-03-26 18:20:10 +0700

    sec

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,18 @@ Route::get('/tasks/{id}', function($id) {
 
 Route::post('tasks', function(Request $request){
     // dd($request->all());
-    $data=$request->validate([
-        'title'=>'required|max:255',
-        'description'=>'required|min:3|max:255',
-        'long_description'=>'required|min:3|max:255',
-    ]);
-    $task = new Task();
-    $task->title = $request->title;
-    $task->description = $request->description;
-    $task->long_description = $request->long_description;
-    $task->completed = false;
-    $task->save();
+    // $data=$request->validate([
+    //     'title'=>'required|max:255',
+    //     'description'=>'required|min:3|max:255',
+    //     'long_description'=>'required|min:3|max:255',
+    // ]);
+    // $task = new Task();
+    // $task->title = $request->title;
+    // $task->description = $request->description;
+    // $task->long_description = $request->long_description;
+    // $task->completed = false;
+    // $task->save();
+    $task = Task::created($request->validate());
     return redirect()->route('tasks.index')->with('success', 'Task created successfully');
 })->name('tasks.store');
 
@@ -40,16 +41,7 @@ Route::get('tasks/{id}/edit', function($id){
 })->name('tasks.edit');
 
 Route::put('tasks/{id}', function($id, Request $request){
-    $data = $request->validate([
-        'title'=>'required|max:255',
-        'description'=>'required|min:3|max:255',
-        'long_description'=>'required|min:3|max:255',
-    ]);
-    $task = Task::findOrFail($id);
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
+    Task::findOrFail($id)-> update($request->validate());
     return redirect()->route('tasks.index')->with('success', 'Task updated successfully');
 })->name('tasks.update');
 
@@ -58,3 +50,7 @@ Route::delete('tasks/{id}', function ($id) {
     $task->delete();
     return redirect()->route('tasks.index')->with('success', 'Task deleted successfully');
 })->name('tasks.delete');
+
+// Route::get('/about', function(){
+//     return view('index',['name'=>'About']);
+// });
